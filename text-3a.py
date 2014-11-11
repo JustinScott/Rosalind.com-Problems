9f065bf4cdf9115877a12e3556efd46f43249fda
Justin Scott <jsco976@gmail.com>
2014-11-11 17:08:30 -0600

    3a

--- text-3a.py
+++ text-3a.py
@@ -4,35 +4,41 @@ def distance(string1, string2):
 
 def gen_kmers(k, d, ref):
     kmers = set()
+    kmers.add(ref)
+    new = set()
     for i in range(0, d):
-        for j in range(0, k):
-            for aa in ['A', 'T', 'G', 'C']:
-                if aa != ref[j]:
-                    kmers.add(ref[0:j] + aa + ref[j+1:])
+        for kmer in kmers.copy():
+            for j in range(0, k):
+                for aa in ['A', 'T', 'G', 'C']:
+                    new.add(kmer[0:j] + aa + kmer[j+1:])
     return kmers
 
 
 def motifenumeration(k, d, dna):
+    out = set()
     for i in range(0, len(dna[0]) - k + 1):
         kmers = gen_kmers(k, d, dna[0][i:i+k])
         for kmer in kmers:
             for j in range(0, len(dna)):
                 match = False
                 for m in range(0, len(dna[j]) - k + 1):
-                    if distance(kmer, dna[j][m:m+k]) == d:
+                    if distance(kmer, dna[j][m:m+k]) <= d:
                         match = True
                         break
                 if not match:
                     break
             else:
-                print(kmer)
+                out.add(kmer)
+    return out
 
 
 def main():
-    data = open('D:/net dlz/rosalind_test.txt').read().split('\n')
+    data = open('D:/net dlz/rosalind_3a.txt').read().split('\n')
     k = data[0].split(' ')[0]
     d = data[0].split(' ')[1]
-    motifenumeration(int(k), int(d), data[1:])
+    kmers = motifenumeration(int(k), int(d), data[1:])
+    for kmer in kmers:
+        print(kmer, end=' ')
 
 
 if __name__ == '__main__':
